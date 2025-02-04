29ca600d3472def7241df0e532ad63d98967786b
Anthony Trépanier <38358928+Rangerz132@users.noreply.github.com>
2025-02-04 10:59:34 -0500

    fix hero-section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -5,32 +5,43 @@ import IntroDesktop from "../assets/images/bg-intro-desktop.svg";
 
 const HeroSection = () => {
   return (
-    <div className="relative">
-      <div className="relative flex flex-col space-y-10  md:space-y-0 wrapper md:flex-row-reverse md:justify-between  md:space-x-10">
+    <div className="relative ">
+      <div className="relative flex flex-col space-y-10  md:space-y-0 wrapper  md:space-x-10">
         {/** Mockups */}
-        <div className="md:basis-1/2">
+        <div className="md:basis-1/2 md:absolute md:right-0 md:top-[25%] md:w-[40%] lg:w-[35%]">
           <img src={Mockups} alt="Mockups" className="" />
         </div>
 
         {/** Content Section */}
-        <div className="flex flex-col items-center justify-center text-center space-y-8 md:items-start md:text-left md:basis-2/5">
-          {/** Title */}
-          <h1 className="text-4xl text-primary-dark-blue md:text-6xl">
-            Next generation <br /> digital banking
-          </h1>
-          {/** Description */}
-          <p className="text-neutral-grayish-blue">
-            Take your financial life online. Your Easybank account will be a
-            one-stop-shop for spending, saving, budgeting, investing, and much
-            more.
-          </p>
-          {/** Button */}
-          <Button text="Request invite" onButtonClick={() => {}} />
+        <div className="relative md:left-0 md:max-w-[50%] md:pr-10 md:mt-40">
+          <div className="flex flex-col items-center justify-center text-center space-y-8 md:items-start md:text-left ">
+            {/** Title */}
+            <h1 className="text-4xl text-primary-dark-blue md:text-6xl">
+              Next generation <br /> digital banking
+            </h1>
+            {/** Description */}
+            <p className="text-neutral-grayish-blue">
+              Take your financial life online. Your Easybank account will be a
+              one-stop-shop for spending, saving, budgeting, investing, and much
+              more.
+            </p>
+            {/** Button */}
+            <Button text="Request invite" onButtonClick={() => {}} />
+          </div>
         </div>
       </div>
+
+      {/** Intro Shapes Desktop*/}
       <div className="hidden md:flex ">
-        <div className="absolute top-0 right-0 translate-x-[20%] -z-10 ">
-          <img src={IntroDesktop} alt="Intro mobile" className="" />
+        <div className=" absolute md:top-[30%] right-0 -z-10 md:max-w-[50%] lg:max-w-[55%] ">
+          <img src={IntroDesktop} alt="Intro desktop" className="" />
+        </div>
+      </div>
+
+      {/** Intro Shapes Mobile*/}
+      <div className="flex md:hidden ">
+        <div className="absolute top-0 w-full -z-10 ">
+          <img src={IntroMobile} alt="Intro mobile" className="w-full" />
         </div>
       </div>
     </div>
